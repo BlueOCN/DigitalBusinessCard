89d6da25a992cad6a99f33077ba3a64abb2ddcc3
BlueOCN <robgonzalezmu@gmail.com>
2024-06-27 11:11:37 -0600

    Update fixed asset load error

--- digitalBusinessCardD/src/components/Footer.jsx
+++ digitalBusinessCardD/src/components/Footer.jsx
@@ -1,12 +1,17 @@
+import tw_icon from '../assets/Twitter Icon.png'
+import fb_icon from '../assets/Facebook Icon.png'
+import ig_icon from '../assets/Instagram Icon.png'
+import in_icon from '../assets/Linkedin Icon.png'
+import gh_icon from '../assets/GitHub Icon.png'
 
 function Footer() {
     return (
         <div className="shoes">
-            <img src="./src/assets/Twitter Icon.png" alt="tw_icon" />
-            <img src="./src/assets/Facebook Icon.png" alt="fb_icon" />
-            <img src="./src/assets/Instagram Icon.png" alt="ig_icon" />
-            <img src="./src/assets/Linkedin Icon.png" alt="in_icon" />
-            <img src="./src/assets/GitHub Icon.png" alt="gh_icon" />
+            <img src={tw_icon} alt="tw_icon" />
+            <img src={fb_icon} alt="fb_icon" />
+            <img src={ig_icon} alt="ig_icon" />
+            <img src={in_icon} alt="in_icon" />
+            <img src={gh_icon} alt="gh_icon" />
         </div>
     )
 }
