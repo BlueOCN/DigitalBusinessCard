344d1490f7f5f3ebd1f9205b91ead95f8c8b461f
BlueOCN <robgonzalezmu@gmail.com>
2024-06-27 11:29:45 -0600

    Update fixed asset load error

--- digitalBusinessCardB/src/components/Footer.jsx
+++ digitalBusinessCardB/src/components/Footer.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import tw_logo from '../assets/Twitter Icon.png'
+import fb_logo from '../assets/Facebook Icon.png'
+import ig_logo from '../assets/Instagram Icon.png'
+import in_logo from '../assets/Linkedin Icon.png'
+import gh_logo from '../assets/GitHub Icon.png'
 
 function Footer () {
     return (
         <footer>
-            <img className="tw--logo" src="./src/assets/Twitter Icon.png" alt="twitter_logo" />
-            <img className="fb--logo" src="./src/assets/Facebook Icon.png" alt="facebook_logo" />
-            <img className="ig--logo" src="./src/assets/Instagram Icon.png" alt="instagram_logo" />
-            <img className="li--logo" src="./src/assets/Linkedin Icon.png" alt="linkedin_logo" />
-            <img className="gh--logo" src="./src/assets/GitHub Icon.png" alt="github_logo" />
+            <img className="tw--logo" src={tw_logo} alt="twitter_logo" />
+            <img className="fb--logo" src={fb_logo} alt="facebook_logo" />
+            <img className="ig--logo" src={ig_logo} alt="instagram_logo" />
+            <img className="li--logo" src={in_logo} alt="linkedin_logo" />
+            <img className="gh--logo" src={gh_logo} alt="github_logo" />
         </footer>
     )
 }
